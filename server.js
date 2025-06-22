16b47c3ee5eab9d3897c38c34fdb9d1c2cfc33a6
shrijith218 <shrijithsankaran218@gmail.com>
2025-06-22 20:57:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // ✅ Supabase setup
 const supabaseUrl = 'https://ieqlswwdfobuuahxyowh.supabase.co';
-const supabaseKey = 'YOUR_PUBLIC_ANON_KEY'; // Keep secret keys out of public code
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllcWxzd3dkZm9idXVhaHh5b3doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1OTU2MTQsImV4cCI6MjA2NjE3MTYxNH0.kVfRidaDIH-uABmkbWf7yr0YlZmRkbtOuGFnN2KePFI;' // Keep secret keys out of public code
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // === POST /scan: Log scanned data ===
